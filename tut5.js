9bc3cac4aaf7f71096ace356aaae03e0210f026b
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-09-15 18:51:10 +0530

    Add files via upload

---
+++ tut5.js
@@ -0,0 +1,11 @@
+//strings
+//anything under a parenthesis "" or '' is a string
+//for example:
+let a = "1,2,3,4";
+let b = 'a,b,c,d';
+
+//in a string each element has a index number starting from left 
+// in a index of 1 is 0 for 2 its 1 and so on
+
+a.length; //.length gives us the length of the string;
+a[0]; //gives us the element in the index number 0 of a
---
+++ tut8.js
@@ -0,0 +1,9 @@
+//null and undefined
+
+
+//null
+//   intentional absense of any value
+//   must be assigned
+
+//undefined
+//    variable that do not have an assigned value is undefined
