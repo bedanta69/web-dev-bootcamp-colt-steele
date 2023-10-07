cf35befdddeefc2dc11d899e7dd9d1a9ba41759c
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-07 21:20:56 +0530

    Add files via upload

---
+++ push&pop.js
@@ -0,0 +1,10 @@
+//push is a method used to add something at the end of an array
+//pop is a method used to remove something at the end of an arrey
+let a = [1, 2, 3, 4, 5];
+//suppose we are to add 6 at the end of a 
+a.push(6); //adds 6 at the end of an array
+//suppose we are to remove 6 now from the end 
+a.pop();
+
+
+
