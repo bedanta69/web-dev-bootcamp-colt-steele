d27c8aeea04e50d9cc82b1c3dbbb9911d36bf9bc
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-08 18:20:53 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+let a = parseInt(prompt("Enter a Number",))
+if (a % 2 === 0) {
+    console.log(a, " is even");
+
+} else {
+    console.log(a, " is odd"); 14
+
+}
