be035598f90ac8455f873f2cade691c6262da3d6
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-09 23:03:00 +0530

    Add files via upload

--- concat.js
+++ concat.js
@@ -5,3 +5,10 @@ let array2 = [6, 7, 8, 9, 10];
 
 let array3 = array1.concat(array2);
 //expected output = [1,2,3,4,6,7,8,9,10]
+
+//includes is a boolean method that includes only two values true or false
+console.log(array1.includes(8))
+//expected output: false
+
+console.log(array1.includes(1))
+//expected output: true
