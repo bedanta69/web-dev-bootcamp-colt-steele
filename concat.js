ca97817c21db43004f3b2795f20d27c0e8bdacea
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-09 22:37:03 +0530

    Add files via upload

---
+++ concat.js
@@ -0,0 +1,7 @@
+//concat is a method to merge two arrays
+
+let array1 = [1, 2, 3, 4];
+let array2 = [6, 7, 8, 9, 10];
+
+let array3 = array1.concat(array2);
+//expected output = [1,2,3,4,6,7,8,9,10]
