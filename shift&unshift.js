5b611f6b84f546250e20afe1f18d27877bed77a2
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-11 12:57:20 +0530

    Delete shift&unshift.js

--- shift&unshift.js
+++
@@ -1,6 +0,0 @@
-//shift is a method used to remove something at the begening of an array
-//unshift is a method to add something from the begening of an array
-let a = [1, 2, 3, 4, 5];
-a.unshift(0);
-a.shift()
-console.log(a);
