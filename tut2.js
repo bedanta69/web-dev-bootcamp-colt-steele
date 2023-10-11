b119db5a1cc7c1e1587514641aa3a1397b565995
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-11 12:58:10 +0530

    Delete tut2.js

--- tut2.js
+++
@@ -1,6 +0,0 @@
-//difference between ++i and i++
-let i = 4;
-let result = i++; // i++ : post incriment operator(updates the value after storing the value in result)
-result = ++i // ++i : pre increment operator (updates the value before storing the value in result)
-console.log(result);
-console.log(i)
