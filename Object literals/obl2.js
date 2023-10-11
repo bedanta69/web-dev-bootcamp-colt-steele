d9b82c40a0240126862e68e78710d5cf45f12a3c
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-11 23:08:15 +0530

    Add files via upload

---
+++ Object literals/obl2.js
@@ -0,0 +1,9 @@
+//accessing an element out of a object literal
+const objectliteral = {
+    firstname: "Bedanta",
+    lastname: "Kashyap"
+};
+console.log(objectliteral["firstname"]);
+//expected output:Bedanta
+console.log(objectliteral["lastname"]);
+//expected output:Kashyap
