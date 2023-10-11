d52ec93f64ab3c57685e0add5d81498c0a9be540
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-11 12:57:37 +0530

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync')();
-let a = parseInt(prompt("Enter a Number",))
-if (a % 2 === 0) {
-    console.log(a, " is even");
-
-} else {
-    console.log(a, " is odd"); 14
-
-}
