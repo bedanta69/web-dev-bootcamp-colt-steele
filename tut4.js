bcf7ca23098571dcd793aad08e454b2139ff05a6
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-11 12:58:18 +0530

    Delete tut4.js

--- tut4.js
+++
@@ -1,4 +0,0 @@
-//there is a data type called boolean and it can have only two values
-let a = true; // value of a is true
-let b = false; //value of b is false 
-// in javascript we can also change the data types of a variable for exaple if a variable is used for storing a constant we can later on change its values to a boolean
