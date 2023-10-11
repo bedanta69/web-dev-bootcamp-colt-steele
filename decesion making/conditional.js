9734245fe9a6875f9cbab2005354ee5b663a93d8
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-11 12:48:16 +0530

    Add files via upload

---
+++ decesion making/conditional.js
@@ -0,0 +1,11 @@
+let a = Math.floor(Math.random() * 2) + 1;
+
+if (a == 1) {
+    console.log("Your number is one");
+}
+
+else {
+    console.log("Your number is two");
+}
+
+//else if is also a conditional used to add more conditions in between if and else
