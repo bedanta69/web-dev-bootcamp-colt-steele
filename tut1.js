503f99c6414cce15de9087aff5fece546ea05517
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-11 12:57:55 +0530

    Delete tut1.js

--- tut1.js
+++
@@ -1,6 +0,0 @@
-//variables and updating variables
-let a; //created a variable by the name a
-a = 4; //assigned a a value of 4
-
-a += 6; //increments the value of a by 6
-a -= 2; //decrements the value of a by 2
