590eb2147ba29887735c91e726efe258ef81e63f
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-16 22:25:55 +0530

    Add files via upload

---
+++ loops/for of.js
@@ -0,0 +1,10 @@
+// for of loop executes a loop that works on a source of data sourced from an itterable object
+// syntax
+// for (variable of iterable){ 
+//    statement;}
+
+const array1 = ['a', 'b', 'c', 'd', 'e'];
+
+for (const element of array1) {
+    console.log(element);
+} 
