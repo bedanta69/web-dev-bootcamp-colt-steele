3b697c66f80b6c3bba1fb26002e80182854dd512
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-16 23:09:11 +0530

    Add files via upload

---
+++ loops/iterating objects.js
@@ -0,0 +1,11 @@
+const testscores = {
+    babykeem: 90,
+    kendrick: 99,
+    carti: 100,
+    frankocean: 88,
+
+}
+
+for (const scores in testscores) {
+    console.log(scores, " ", "marks", " ", "=", " ", testscores[scores]);
+}
