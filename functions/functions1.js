3f2509babadc3c777ec48805195c470b4343c265
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-21 00:10:08 +0530

    Add files via upload

--- functions/functions1.js
+++ functions/functions1.js
@@ -1,3 +1,13 @@
 //There are two steps with functions 
 //Step 1: Defining a function 
 //step 2: Running the function
+
+//defining the function
+function grumps() {
+    //telling the function what tasks to do
+    console.log('Ugh...you again');
+    console.log('for the last time...');
+    console.log('Leave me alone!!!');
+};
+
+grumps();  //running the funcction
