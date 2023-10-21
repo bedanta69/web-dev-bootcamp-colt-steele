fdb59e8b153fc447205e1e389ed7baf847d97f02
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-22 03:57:35 +0530

    Add files via upload

---
+++ exercises/exercise40.js
@@ -0,0 +1,10 @@
+// DEFINE YOUR FUNCTION BELOW:
+function lastElement(array) {
+
+    if (array.length == 0) {
+        return null;
+
+    } else {
+        return array[array.length - 1]
+    }
+}
