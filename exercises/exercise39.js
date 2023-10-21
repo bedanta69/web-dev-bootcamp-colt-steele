34edcc7199ff36f0c3b1942f050d0bc1b4e4f6ce
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-22 02:17:46 +0530

    Add files via upload

---
+++ exercises/exercise39.js
@@ -0,0 +1,9 @@
+// DEFINE YOUR FUNCTION BELOW:
+function isShortsWeather(temperature) {
+    parseInt(temperature);
+    if (temperature >= 75) {
+        return true;
+    } else {
+        return false;
+    }
+};
