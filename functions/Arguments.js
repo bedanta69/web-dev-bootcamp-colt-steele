acfeac4329c8b4cb9c7f3a21cf52fe10dfb4589f
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-21 23:50:17 +0530

    Add files via upload

---
+++ functions/Arguments.js
@@ -0,0 +1,7 @@
+//Arguments are values passed in to a function to obtain their values
+function greet(FIrstperson) {
+    console.log("hello", " ", FIrstperson);
+};
+
+greet("brian");
+//Expected output : hello brian
