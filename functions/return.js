304a852078a234ea1ac93d85bd2b8ba42751fdd5
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-22 01:57:54 +0530

    Add files via upload

---
+++ functions/return.js
@@ -0,0 +1,6 @@
+//return values store the function values while console.log prints the value as a string which cant be used further for other purposes
+//return only returns a single value
+function sum(x, y) {
+    return x + y; //stores the value of x+y
+};
+sum(1, 2)
