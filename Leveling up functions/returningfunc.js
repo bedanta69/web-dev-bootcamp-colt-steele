9ba97a70691cc738b8e6290b41063a5302ec846c
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2023-11-17 20:46:34 +0530

    Add files via upload

--- Leveling up functions/returningfunc.js
+++ Leveling up functions/returningfunc.js
@@ -1,4 +1,15 @@
 //returning functions
-function isbetwwen(num) {
-    return num >= 50 && num <= 100;
+function makemysteryfunction() {
+    const random = Math.random();
+    if (random > 0.5) {
+        return function () {
+            console.log("I am a good function");
+        }
+    } else {
+        return function () {
+            alert("I am a bad function you have bben infected by a computer virus")
+        }
+    }
 }
+makemysteryfunction()
+//here it returns either of the two functions depending on the Math.random factor
