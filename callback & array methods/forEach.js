17c040543676f97473e66d580caac883b74c34d5
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2023-12-01 22:52:39 +0530

    Add files via upload

---
+++ callback & array methods/forEach.js
@@ -0,0 +1,12 @@
+//accepts a callback function
+//calls the function once per element in an array
+const numbers = [1, 2, , 3, 4, 5, 6, 7, 8, 9, 10];
+function print(element) {
+    console.log(element);
+};
+numbers.forEach(print)
+
+//but it is most commonly used like this
+numbers.forEach(function (el) {
+    console.log(el)
+})
