c5afcbe292d693b8bf712a91843f393e85a141e7
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2023-12-16 10:22:05 +0530

    Add files via upload

--- callback & array methods/arrowfunc.js
+++ callback & array methods/arrowfunc.js
@@ -5,3 +5,11 @@ const sum = (x, y) => {
 }
 console.log(sum(1, 2));
 //expected output 3
+
+//implicit returns for arrow functions
+const multiply = (x, y) => (
+    x * y                                // we dont need to specify the return keyword when using the first bracke or no brackets at all for arrow functions
+)
+
+console.log(multiply(2, 4))
+//expected output 8
