ec566e9a63c2153ca8b6311ba26b0b77ae71ab01
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2024-01-24 13:25:56 +0530

    Add files via upload

---
+++ Newer Javascript Features/spread_arr.js
@@ -0,0 +1,7 @@
+//Spread with array literals
+const cats = ["Mika", "Tom", "Marowel"];
+const dogs = ["Tommy", "Bull", "Gimmy"];
+
+//merging the two arrays into a single array called pets
+let pets = [...cats, ...dogs]
+
