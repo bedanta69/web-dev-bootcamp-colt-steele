bb59e1e9cf8e5e43d271a3a2703b28d52a9a6873
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2024-01-24 14:31:17 +0530

    Add files via upload

---
+++ Newer Javascript Features/Rest_params.js
@@ -0,0 +1,5 @@
+function raceResults(gold, silver, ...everyoneElse) {
+    console.log(`Gold medal goes to ${gold}`)
+    console.log(`Silver medal goes to ${silver}`)
+    console.log(`And thanks to Everyoneelse ${everyoneElse}`)
+}
