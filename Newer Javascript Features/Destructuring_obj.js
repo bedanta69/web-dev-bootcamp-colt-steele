cec0b349704ba7ebc8a917764d55456c3873b57d
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2024-01-24 15:45:35 +0530

    Add files via upload

---
+++ Newer Javascript Features/Destructuring_obj.js
@@ -0,0 +1,9 @@
+//destructuring objects into distinct variables
+const user = {
+    FirstName: "Bedanta",
+    LastName: "Kashyap",
+    email: "bedantakashyap75@gmail.com",
+
+}
+
+const { email } = user;
