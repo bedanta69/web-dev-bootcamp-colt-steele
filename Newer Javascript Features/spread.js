b3f5e7aee76b0ed8019eae11010e4f33256d361b
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2024-01-24 13:04:41 +0530

    Delete Newer Javascript Features/spread.js

--- Newer Javascript Features/spread.js
+++
@@ -1,8 +0,0 @@
-//Spread for function calls
-//Expands an iterable(array,strings etc ) into a list of arguments
-const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(Math.max(num));
-//Expected output NaN
-
-console.log(Math.max(...num))//converts the num array into an argument for the Math.max() funcn
-//expected output 10;
