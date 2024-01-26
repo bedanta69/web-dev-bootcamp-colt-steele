81d7a13d314b9d35ea2a679185208c466c474b1d
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2024-01-26 20:20:03 +0530

    Delete html forms and tables/Destructuring_arr.js

--- html forms and tables/Destructuring_arr.js
+++
@@ -1,4 +0,0 @@
-//destructuring arrays into distinct variables
-const scores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const [gold, silver, bronze, ...everyoneElse] = scores;
-//saves 1 to gold , 2 to silver , 3 to bronze and so on
